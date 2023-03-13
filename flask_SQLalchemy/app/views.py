a57d9139f049c56c8ce27ac54477622f061e9875
David Masila <davidmasila97@gmail.com>
2023-03-13 12:32:40 -0700

    Flask SQLAlchemy

--- flask_SQLalchemy/app/views.py
+++ flask_SQLalchemy/app/views.py
@@ -1,1 +1,31 @@
 from flask import redirect, url_for, request, render_template
+from app import app
+from app.models import Students, db
+from app.forms import Student
+
+@app.route("/")
+def index():
+    students =  Students.query.all()
+    return render_template('/index.html', students = students)
+
+@app.route("/<int:student_id>")
+def student(student_id):
+    student =  Students.query.get_or_404(student_id)
+    return render_template("/student.html", student=student)
+
+@app.route("/create", methods=['GET','POST'])
+def create():
+    form =  Student()
+    if form.validate_on_submit():
+        student = Students(
+            firstname = form.firstname.data,
+            lastname = form.lastname.data,
+            email = form.email.data,
+            age = int(form.age.data),
+            bio = form.bio.data
+        )
+        db.session.add(student)
+        db.session.commit()
+        form.process()
+        return redirect(url_for('index'))
+    return render_template("/create.html", form = form)
